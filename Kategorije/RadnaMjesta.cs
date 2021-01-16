02ec8b17be7615c3f8b521566f3b33c37948f3a9
Roko2 <47241597+Roko2@users.noreply.github.com>
2021-01-16 21:58:13 +0100

    Add files via upload

---
+++ Kategorije/RadnaMjesta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RadnaMjesta
+    {
+        public int IdPosao { get; set; }
+        public string NazivPosao { get; set; }
+        public DateTime Datum { get; set; }
+        public string OpisPosla { get; set; }
+
+    }
+}

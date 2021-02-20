748a84fd9db8413bcb8a0fe64fb92f1f31816a1e
Roko2 <47241597+Roko2@users.noreply.github.com>
2021-02-20 14:39:18 +0100

    Add files via upload

---
+++ HZZ_Kovac/ConsoleAppTester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Reflection;
+using DataAccess;
+
+namespace ConsoleAppTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MainRepository objekt = new MainRepository();
+            //List<Category> usersREST = objekt.GetCategories();
+            //for (int i = 0; i < usersREST.Count; i++)
+            //{
+            //    Console.WriteLine("ID " + usersREST[i].ID + " NAZIV KATEGORIJE: " +usersREST[i].Description+"URL: "+usersREST[i].Url);
+            //}
+            String URLString = "http://burzarada.hzz.hr/rss/rsskat1001.xml";
+            XDocument xdoc = XDocument.Load(URLString);
+
+
+            Console.ReadKey();
+        }
+    }
+            
+   
+        }
+
